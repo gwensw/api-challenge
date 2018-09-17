eca0b5c8c8fa014c59ee9e8f0f75c730affb2c9c
gwensw <gwen.simswilliams@gmail.com>
2018-09-17 18:51:51 +0100

    Homepage and day 1 project

---
+++ day1/day1.js
@@ -0,0 +1,23 @@
+function randomGif(tag) {
+  fetch(`http://api.giphy.com/v1/gifs/random?tag=${tag}&rating=G&api_key=6zhKqvUGeYCpwndY2geuy4cEe0g7dRRv`)
+    .then(res => res.json())
+    .then((res) => {
+      document.getElementById('gif').src = res.data.images.fixed_height.url;
+    })
+    .catch((err) => {
+      // TODO: render a sad message on the screen
+      console.log(err);
+    });
+}
+
+['click', 'keydown'].forEach((event) => {
+  document.addEventListener(event, (e) => {
+    const el = e.target;
+    if (el.classList.contains('controller__button')) {
+      randomGif(el.dataset.tag);
+      Array.from(document.querySelectorAll('.controller__button--selected'))
+        .forEach(button => button.classList.remove('controller__button--selected'));
+      el.classList.add('controller__button--selected');
+    }
+  });
+});
